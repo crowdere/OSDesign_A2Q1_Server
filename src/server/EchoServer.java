ae17e8ef8d8d414b667adb067868ed30e35677c5
eddyc <eddy.crowder@gmail.com>
2018-09-28 15:52:04 -0400

    added comments

--- src/server/EchoServer.java
+++ src/server/EchoServer.java
@@ -12,31 +12,38 @@ import java.net.SocketException;
 
 /**
  *
- * @author eddyc
+ * @author eddyc & michaela
  * @version 1.0
  * @description Opens a connection for the client to connect to. Echos back all details and 
  *              exits when the client terminates the connection.
  */
 public class EchoServer
 {
+    //int PORT = port to listen on
     EchoServer(int PORT)
     {
+        //client id variable 
         String client_id = null;
         System.out.println("Server started on port " + PORT);
         try
         {
+          //open serversocket for listening on
           ServerSocket server = new ServerSocket(PORT);  
+          
           while(true)
           {
             try (Socket client = server.accept()) 
             {
+                //In/out iostreams for communication
                 BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                 PrintWriter out = new PrintWriter( new OutputStreamWriter(client.getOutputStream()));
 
+                //Main loop for receiving and replying to data
                 String received;
                 while(true)
                 {
                     received = in.readLine();
+                    //create the client ID on the first contact
                     if(client_id == null)
                     {
                         client_id = received;
@@ -52,6 +59,7 @@ public class EchoServer
             }
             catch(SocketException e)
             {
+                //Client must of terminated connection, CLEAN UO
                 System.out.println("Client " + client_id + " has disconnected!");
                 client_id = null;
             }
@@ -59,10 +67,11 @@ public class EchoServer
         }
         catch (IOException ioex)
         {
-
+            //@TODO Handle IOException
         }
     }
-
+    
+    //Driver function to test echoServer functions
     public static void main(String args[])
     {
        //Default values
